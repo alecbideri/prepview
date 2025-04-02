521928bb003704a374e896a6c042e0035182bebb
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-04-02 18:16:04 +0200

    Created user authentication in the firebase and modfied the All layouts , plus authform

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,6 +9,12 @@ import { Form } from "@/components/ui/form";
 import { toast } from "sonner";
 import FormField from "@/components/FormField";
 import { useRouter } from "next/navigation";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth..action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -33,16 +39,53 @@ const AuthForm = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
+        const { name, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password,
+        );
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result?.success) {
+          toast.success(result?.message);
+        }
         toast.success("Account created successfully !!");
         router.push("/sign-in");
       } else {
+        const { email, password } = values;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password,
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error("Sign in failed");
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
         toast.success("Sign in successfully .");
         router.push("/");
       }
-    } catch (error) {
+    } catch (error: any) {
       console.log(error);
       toast.error(`There was a problem with error: ${error.message}`);
     }
