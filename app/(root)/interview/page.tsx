785d09ab64b974688b7371b7ca79c9ae70fac407
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-04-03 13:01:34 +0200

    Fixed a typo

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,7 +4,7 @@ import Agent from "@/components/Agent";
 const Page = () => {
   return (
     <>
-      <h3>Interview Gneration</h3>
+      <h3>Interview Generation</h3>
       <Agent userName="You" userId="user1" type="generate" />
     </>
   );
