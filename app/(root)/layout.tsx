c3c09ae05812f97eb5f992c0fec6da1ca7c98a2a
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-04-05 11:30:56 +0200

    Changed the route direction of the navigation to the dahsboard main file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -10,7 +10,7 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
   return (
     <div className="root-layout">
       <nav>
-        <Link href="/public" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="logo" width={38} height={32} />
           <h2 className="text-primary-100">PrepView</h2>
         </Link>
